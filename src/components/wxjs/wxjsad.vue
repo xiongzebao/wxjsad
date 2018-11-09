3cea6fb164fb3ea8f7f95dc5fb0229556b2eab0b
xiongbin <01549685@yto.net.cn>
2018-11-09 16:06:08 +0800

    add wxjs add

--- src/components/wxjs/wxjsad.vue
+++ src/components/wxjs/wxjsad.vue
@@ -1,45 +1,78 @@
-
+<style type="text/css">
+  .wxjs-img {
+    width: 100%;
+  }
+  .title{
+    font-weight: bold;
+  }
+  .desc{
+    font-size: 0.25rem;
+  }
+</style>
 <template>
   <div id='root'>
-  wxjsadd
-    
+    <div v-for="(item,index) in list" v-bind:key="index">
+      <h1 class="title">{{index+1}}、{{item.title}}</h1>
+      <p class="desc">{{item.desc}}</p>
+      <img class="wxjs-img" :src="item.imgUrl" />
+    </div>
   </div>
-
 </template>
 
 
 <script>
- import Vue from 'vue'
-import { MessageBox } from 'mint-ui';
-
-export default {
-  name: 'app',
-  beforeCreated(){
- 
-  },
-
-  mounted(){
-
-  },
-  computed:{
-  
-  
-  },
-  data(){
-    return{
-      
-    }
-
-  },
-  methods:{
-   
-  },
-  components:{
-
+  import Vue from 'vue'
+  import {
+    MessageBox
+  } from 'mint-ui';
+  export default {
+    name: 'app',
+    beforeCreated() {},
+    mounted() {
+      this.list.push({
+        imgUrl: "../../../static/images/wxjsad/share1.jpg",
+        desc: "首页",
+        title: "首页"
+      })
+      this.list.push({
+        imgUrl: "../../../static/images/wxjsad/share2.jpg",
+        desc: "统计页面",
+        title: ""
+      })
+      this.list.push({
+        imgUrl: "../../../static/images/wxjsad/share4.jpg",
+        desc: "个人中心",
+        title: ""
+      })
+      this.list.push({
+        imgUrl: "../../../static/images/wxjsad/share5.jpg",
+        desc: "",
+        title: ""
+      })
+      this.list.push({
+        imgUrl: "../../../static/images/wxjsad/share6.jpg",
+        desc: "",
+        title: ""
+      })
+      this.list.push({
+        imgUrl: "../../../static/images/wxjsad/share7.jpg",
+        desc: "",
+        title: ""
+      })
+      this.list.push({
+        imgUrl: "../../../static/images/wxjsad/wxjs_share_ad.png",
+        desc: "",
+        title: ""
+      })
+    },
+    computed: {},
+    data() {
+      return {
+        list: []
+      }
+    },
+    methods: {},
+    components: {}
   }
-}
 </script>
 
-<style type="text/css">
-
-</style>
